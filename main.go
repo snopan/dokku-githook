0a748fd06546e89ca2d4dcd282e673f2ec028f9a
snopan <snorepan@gmail.com>
2021-11-06 14:32:44 +1100

    minor fix attempt

--- main.go
+++ main.go
@@ -130,6 +130,6 @@ func main() {
 
 	// Start the http server
 	log.Print(fmt.Sprintf("Trying to bind to port %s!", os.Getenv("GITHUB_HOOK_PORT")))
-	http.ListenAndServe(":123", nil)
+	http.ListenAndServe(":4000", nil)
 	log.Print(fmt.Sprintf("Github hook http server has started on port %s!", os.Getenv("GITHUB_HOOK_PORT")))
 }
