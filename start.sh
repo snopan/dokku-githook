6debb5721dbd46b853c2f76d06efc6e25bde408f
snopan <snorepan@gmail.com>
2021-12-09 00:06:06 +1100

    minor fix attempt

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 start() {
-	declare GITHUB_USERNAME=$1
-	declare GITHUB_TOKEN=$2
-	declare GITHUB_HOOK_PORT=$3
-	declare LOCAL_CONTROL_PORT=$4
-	echo "$GITHUB_USERNAME $GITHUB_TOKEN $GITHUB_HOOK_PORT $LOCAL_CONTROL_PORT"
+	export GITHUB_USERNAME=$1
+	export GITHUB_TOKEN=$2
+	export GITHUB_HOOK_PORT=$3
+	export LOCAL_CONTROL_PORT=$4
 	/home/dokku/go/bin/go run ./main.go	
 }
 
