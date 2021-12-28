a6827ed588d86cacf2ed674e1d444a2bd4032477
snopan <snorepan@gmail.com>
2021-12-28 20:12:42 +1100

    minor fix attempt

--- start.sh
+++ start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-start() {
-	export GITHUB_USERNAME=$1
-	export GITHUB_TOKEN=$2
-	export GITHUB_HOOK_PORT=$3
-	export LOCAL_CONTROL_PORT=$4
-	echo "testing $GITHUB_USERNAME"
-	/home/dokku/go/bin/go run ./main.go	$GITHUB_USERNAME $GITHUB_TOKEN
-}
-
-echo "all args $@"
-start "$@"
+echo "args $LOCAL_CONTROL_PORT $GITHUB_HOOK_PORT"
+/home/dokku/go/bin/go run ./main.go
